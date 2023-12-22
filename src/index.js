8a3b1bbc0d2c47ec84d4476900dc32bea55939f5
ellieforbes <139644804+ellieforbes@users.noreply.github.com>
2023-12-22 19:28:18 +0000

    Added AI API integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-function generatePoem(event) {
-    event.preventDefault();
+function displayPoem(response) {
 
     new Typewriter("#poem", {
-        strings: "La tombe dit à la rose",
+        strings: response.data.answer,
         autoStart: true,
         delay: 1,
         cursor: "",
-      });
+    });
+}
+
+function generatePoem(event) {
+    event.preventDefault();
+
+let userInput = document.querySelector("#user-input");
+let apiKey = "b5a70e3dbaf3379o5576fffe161ca0t4";
+let context = "You are a romantic Poem expert and love to write short poems. Your aim is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem.";
+let prompt = `User Instructions: Generate a poem about ${userInput.value}`;
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
